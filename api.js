77170ff44ccd6065198b3057e3ad7dcb2ddd0209
Skovich <83189155+ge96fuj@users.noreply.github.com>
2025-03-01 14:13:06 +0100

    update git.ignore

---
+++ api.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.json());
+
+
+
+// GET route to get the current status of the traffic light
+app.get('/status/A', (req, res) => {
+    console.log('current STATUS OF A is :',global.A_Status);
+
+});
+
+app.get('/status/B', (req, res) => {
+    console.log('current STATUS OF B is :',global.B_Status);
+
+});
+
+app.get('/changeGreen/A', (req, res) => {
+    console.log('Changing Status ');
+    global.interrupt = 0x50 ;
+    //if there is already an interrupt . we should handle that
+
+});
+
+app.get('/changeGreen/B', (req, res) => {
+    console.log('Changing Status ');
+    global.interrupt = 0x51 ;
+
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
+// Start the API server
+app.listen(port, () => {
+  console.log(`API server running at http://localhost:${port}`);
+});
